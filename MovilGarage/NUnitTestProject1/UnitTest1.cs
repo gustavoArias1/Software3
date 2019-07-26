15126c79ca22714b742fb6b52211857a0bb96f13
gustavo andres arias loaiza <guandres-94@hotmail.com>
2019-07-26 16:36:35 -0500

    documnetacion clase de pruebas

--- MovilGarage/NUnitTestProject1/UnitTest1.cs
+++ MovilGarage/NUnitTestProject1/UnitTest1.cs
@@ -6,12 +6,20 @@ using System.Collections.Generic;
 
 namespace Tests
 {
+    /// <summary>
+    /// clase de pruebas unitarias para los metodos mas importantes de toda la logica del negocio
+    /// @ author Gustavo Andres Arias Loaiza
+    /// @ version 1.0
+    /// </summary>
     [TestFixture]
     public class Tests
     {
 
 
 
+        /// <summary>
+        ///  prueba de ingreso al sistema con datos correctos
+        /// </summary>
         [TestCase]
         public void IngresoSistemaTest()
         {
@@ -19,12 +27,12 @@ namespace Tests
             string Usuario = "123@gmail.com";
             string Contraseña = "UZF02FOO5RZ";
             bool entro = true;
-
             Assert.AreEqual(entro, I.auntenticarseSistema(Usuario, Contraseña));
-
-
-
         }
+
+        /// <summary>
+        ///  prueba de ingreso al sistema con contraseña incorrecta
+        /// </summary>
 
         [TestCase]
         public void NoIngresoSistemaTest()
@@ -38,6 +46,25 @@ namespace Tests
 
         }
 
+
+        /// <summary>
+        /// prueba de  ingreso al sistema con corre incorecto 
+        /// </summary>
+        [TestCase]
+        public void NoIngresoSistemaTest2()
+        {
+            IngresarAlSistema I = new IngresarAlSistema();
+            string Usuario = "123@gmail.org";
+            string Contraseña = "UZF02FOO5RZ";
+            bool entro = false;
+            Assert.AreEqual(entro, I.auntenticarseSistema(Usuario, Contraseña));
+
+
+        }
+
+        /// <summary>
+        /// prueba de obtencion de concesionarios por medio de la base datos 
+        /// </summary>
         [TestCase]
         public void UbicarConncesionarioTest()
         {
