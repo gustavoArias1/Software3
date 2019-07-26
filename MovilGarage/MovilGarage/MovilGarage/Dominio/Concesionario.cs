ea04d0a49f91fc3c3d70e51454cb0c8efa963e9b
cristian camilo diaz de los rios <cristiancamilo611@hotmail.com>
2019-07-26 11:16:26 -0500

    AgregarAtributoConsecionario

--- MovilGarage/MovilGarage/MovilGarage/Dominio/Concesionario.cs
+++ MovilGarage/MovilGarage/MovilGarage/Dominio/Concesionario.cs
@@ -7,12 +7,17 @@ namespace MovilGarage.Dominio
     public class Concesionario
     {
         public string nombreConcesionario { get; set; }
+     
+        public string direccion { get; set; }
+        public string telefono  { get; set; }
         public double latitud { get; set; }
         public double longitud { get; set; }
 
-        public Concesionario(string nombreConcesionario, double latitud, double longitud)
+        public Concesionario(string nombreConcesionario, string direccion,string telefono, double latitud, double longitud)
         {
             this.nombreConcesionario = nombreConcesionario;
+            this.direccion = direccion;
+            this.telefono = telefono;
             this.latitud = latitud;
             this.longitud = longitud;
         }
