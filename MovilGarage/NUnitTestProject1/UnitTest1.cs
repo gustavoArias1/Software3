16710e5708a3113f8f706093e6227dfe5c928753
gustavo andres arias loaiza <guandres-94@hotmail.com>
2019-07-26 16:51:42 -0500

    mejoras en la la documentacion

--- MovilGarage/NUnitTestProject1/UnitTest1.cs
+++ MovilGarage/NUnitTestProject1/UnitTest1.cs
@@ -11,40 +11,64 @@ namespace Tests
     {
 
 
-
+        /// <summary>
+        /// prueba de ingreso al sistema con correo y contraseña validos
+        /// </summary>
         [TestCase]
         public void IngresoSistemaTest()
         {
-            IngresarAlSistema I = new IngresarAlSistema();
+            IngresarAlSistema ingresarSistema = new IngresarAlSistema();
             string Usuario = "123@gmail.com";
             string Contraseña = "UZF02FOO5RZ";
             bool entro = true;
 
-            Assert.AreEqual(entro, I.AuntenticarseSistema(Usuario, Contraseña));
+            Assert.AreEqual(entro, ingresarSistema.AuntenticarseSistema(Usuario, Contraseña));
 
 
 
         }
 
+        /// <summary>
+        /// prueba de ingreso al sistema con correo valido pero conntraseña no valida
+        /// </summary>
         [TestCase]
         public void NoIngresoSistemaTest()
         {
-            IngresarAlSistema I = new IngresarAlSistema();
+            IngresarAlSistema ingresarSistema = new IngresarAlSistema();
             string Usuario = "123@gmail.com";
             string Contraseña = "prueba";
             bool entro = false;
-            Assert.AreEqual(entro, I.AuntenticarseSistema(Usuario, Contraseña));
+            Assert.AreEqual(entro, ingresarSistema.AuntenticarseSistema(Usuario, Contraseña));
 
 
         }
 
+        /// <summary>
+        /// prueba de ingreso al sistema con contraseña valida pero correo invalido
+        /// </summary>
+        [TestCase]
+        public void NoIngresoSistemaTest2()
+        {
+            IngresarAlSistema ingresarSistema = new IngresarAlSistema();
+            string Usuario = "123@gmail.org";
+            string Contraseña = "UZF02FOO5RZ";
+            bool entro = false;
+            Assert.AreEqual(entro, ingresarSistema.AuntenticarseSistema(Usuario, Contraseña));
+
+
+        }
+
+
+        /// <summary>
+        /// prueba de obtencion de concesionarios de la base de  datos para su posterior ubicacion
+        /// </summary>
         [TestCase]
         public void UbicarConncesionarioTest()
         {
-            UbicarConcesionarios U = new UbicarConcesionarios();
-            ConexionBaseDatos CB = new ConexionBaseDatos();
-            List<Concesionario> listaConcesionarios = CB.tablaConcesionarios();
-            Assert.AreEqual(listaConcesionarios.Count, U.GetConcesionarios().Count);
+            UbicarConcesionarios ubicarConcesionario = new UbicarConcesionarios();
+            ConexionBaseDatos conexionBaseDatos = new ConexionBaseDatos();
+            List<Concesionario> listaConcesionarios = conexionBaseDatos.TablaConcesionarios();
+            Assert.AreEqual(listaConcesionarios.Count, ubicarConcesionario.GetConcesionarios().Count);
 
                  
         }
