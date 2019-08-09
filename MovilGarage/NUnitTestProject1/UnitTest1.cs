492acb139481fb4ad7993c10f23756f71be3c411
gustavo andres arias loaiza <guandres-94@hotmail.com>
2019-08-09 15:36:09 -0500

    pruebas unitarias registrar usuario

--- MovilGarage/NUnitTestProject1/UnitTest1.cs
+++ MovilGarage/NUnitTestProject1/UnitTest1.cs
@@ -72,5 +72,47 @@ namespace Tests
 
                  
         }
+
+        /// <summary>
+        /// prueba de registro de un usuario con todos sus datos validos y que no se encuentra 
+        /// registrado en la base de datos
+        /// </summary>
+
+        [TestCase]
+
+        public void registrarUsuarioValido()
+        {
+            RegistrarNuevoUsuario registrarNuevoUsuario = new RegistrarNuevoUsuario();
+            ConexionBaseDatos conexionBaseDatos = new ConexionBaseDatos();
+            bool Registro = true;
+            Assert.AreEqual(Registro, registrarNuevoUsuario.RegistraUsuario("josemiguel@gmail.com", "12345678", "12345678"));
+        }
+
+        /// <summary>
+        /// prueba de registro de un usuario que ya sae encuentra en la base  de datos
+        /// </summary>
+
+        [TestCase]
+
+        public void registrarUsuarioRegistrado()
+        {
+            RegistrarNuevoUsuario registrarNuevoUsuario = new RegistrarNuevoUsuario();
+            ConexionBaseDatos conexionBaseDatos = new ConexionBaseDatos();
+            bool Registro = false;
+            Assert.AreEqual(Registro, registrarNuevoUsuario.RegistraUsuario("123@gmail.com", "12345678", "12345678"));
+        }
+
+        /// <summary>
+        /// prueba de registro con usuario nuevo pero segunda contraseña invalida
+        /// </summary>
+        [TestCase]
+
+        public void registrarUsuarioSegundaInvalida()
+        {
+            RegistrarNuevoUsuario registrarNuevoUsuario = new RegistrarNuevoUsuario();
+            ConexionBaseDatos conexionBaseDatos = new ConexionBaseDatos();
+            bool Registro = false;
+            Assert.AreEqual(Registro, registrarNuevoUsuario.RegistraUsuario("juan.12@gmail.com", "12345678", "prueba"));
+        }
     }
 }
